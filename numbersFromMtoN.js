192c83a635653b8961345dae6296e21213214586
Desislava Kracheva <desislava.92@abv.bg>
2021-11-01 16:29:02 +0200

    new ex.

---
+++ numbersFromMtoN.js
@@ -0,0 +1,12 @@
+function solve(m,n){
+
+    m = Number(m);
+    n = Number(n);
+
+    for ( let i = m ; i >=n; i--){
+
+        console.log(i)
+    }
+}
+
+solve(6,2)
---
+++ studentInformation.js
@@ -0,0 +1,8 @@
+function solve(name, age, aveGrade){
+
+
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${aveGrade.toFixed(2)}`)
+
+}
+
+solve('John' , 15 , 5.56231)
